61343830c16ead094011e59b50873d6498ccb68e
zhangzy <zhangzy@mail.51.com>
2016-09-29 17:58:30 +0800

    use service

--- AngularTest/assignment1-starter-code/js/app.js
+++ AngularTest/assignment1-starter-code/js/app.js
@@ -1,30 +1,44 @@
 (function () {
 	'use strict';
 	angular.module('LunchCheck',[])
-	.controller('LunchCheckController',LunchCheckController);
+	.controller('LunchCheckController',LunchCheckController)
+	.service('CheckListService',CheckListService);
 
-	LunchCheckController.$inject = ['$scope'];
-	function LunchCheckController($scope){
+	LunchCheckController.$inject = ['$scope','CheckListService'];
+	function LunchCheckController($scope,CheckListService){
 		$scope.menuList = '';
 		$scope.message  = '';
 		$scope.checkMenu = function(){
 			var menuArray = $scope.menuList.split(",");
 			// console.log(menuArray);
+			$scope.message = CheckListService.checkList(menuArray);
+
+		}
+	}
+
+	function CheckListService() {
+		var service = this;
+		service.message = '';
+		
+		service.checkList = function (list) {
+			// body...
 			var j=0;
-			for(var i=0;i<menuArray.length;i++){				
-				if(menuArray[i].trim() != ""){
+			for(var i=0;i<list.length;i++){				
+				if(list[i].trim() != ""){
 					j = j+1;
 				}				
 			}
-
 			if(j == 0){
-				$scope.message = "Please enter data first";
+				service.message = "Please enter data first";
 			}else if(j > 3){
-				$scope.message = "Too much";
+				service.message = "Too much";
 			}else if(j >= 1 && j <=3){
-				$scope.message = "Enjoy!";
+				service.message = "Enjoy!";
 			}
-			// console.log(j);
+			return service.message;
 		}
+
+		
 	}
+
 })();
