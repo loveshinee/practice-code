049861063b4ab7755b0015e8167ba2ea843285d1
Zeyu <zeyu_1203@foxmail.com>
2016-10-16 21:45:44 +0800

    add assigment3

---
+++ AngularTest/assignment3/js/app.js
@@ -0,0 +1,67 @@
+(function () {
+	'use strict';
+	angular.module('NarrowItDownApp',[])
+	.controller('NarrowItDownController',NarrowItDownController)
+	.service('MenuSearchService',MenuSearchService)
+	.directive('foundItems',FoundItems);
+
+	function FoundItems(){
+		var ddo = {
+			templateUrl: 'list.html',
+			scope:{
+				found: '<',
+				onRemove: '&'
+			}	
+		};
+		return ddo;
+	}
+
+	NarrowItDownController.$inject = ['MenuSearchService'];
+	function NarrowItDownController(MenuSearchService){
+		var ctrl  = this;
+		ctrl.found = "";		 
+		ctrl.toFound = function(){
+			var promise = MenuSearchService.getMatchedMenuItems(ctrl.found);
+			// console.log(ctrl.foundList);
+			promise.then(function(response){	
+				ctrl.foundList = checkItem(response.data,ctrl.found);
+				console.log(ctrl.foundList);
+			}).catch(function(error){
+				console.log("Something Wrong");
+			});
+
+		};
+
+		ctrl.removeItem = function(itemIndex){
+			ctrl.foundList.splice(itemIndex,1);
+		};
+
+	}
+
+	MenuSearchService.$inject = ['$http'];
+	function MenuSearchService($http){
+		var service = this;
+		service.getMatchedMenuItems = function(searchTerm){
+			// var foundItems = [];
+			var foundItems = $http({
+				methd: 'GET',
+				url: 'https://davids-restaurant.herokuapp.com/menu_items.json'
+			});
+			return foundItems;
+		} 
+	}
+
+	function checkItem(items,searchItem){
+		var foundItems = [];
+		var itemList  = items.menu_items;
+		// console.log(itemList.length);
+		for(var i=0; i<itemList.length;i++){
+			var name = itemList[i].name;
+			if(name.toLowerCase().indexOf(searchItem) !== -1){
+				foundItems.push(itemList[i]);
+			}
+		}
+		return foundItems;
+	}
+
+})();
